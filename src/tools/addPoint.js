d21689b011732f6bfd915b5610e017c37c498eb6
Ádryan Pablo <apdsom22@gmail.com>
2023-09-18 01:21:26 -0300

    Active changePlayer feature

--- src/tools/addPoint.js
+++ src/tools/addPoint.js
@@ -2,8 +2,6 @@ export default function addPoint(player = 'player1') {
 
     const scorePanel = document.querySelectorAll(`.-${player} .score`)
 
-    console.log(scorePanel)
-
     let playerScore = 0
     scorePanel.forEach((score) => {
 
--- src/tools/verifyActiveCards.js
+++ src/tools/verifyActiveCards.js
@@ -2,21 +2,24 @@ import findImage from './findImage.js'
 import discoverCards from './discoverCards.js'
 import addPoint from './addPoint.js'
 import hideCards from './hideCards.js'
+import changePlayer from './changePlayer.js'
 
 export default function verifyActiveCards() {
 
     const activeCards = document.querySelectorAll('.card-game.-active')
+    const activePlayer = document.querySelector('.arrow-down').classList.contains('-player1') ? 'player1' : 'player2'
 
     if (activeCards.length == 2) {
 
         if (findImage(activeCards[0]) == findImage(activeCards[1])) {   // Player find
 
             discoverCards(activeCards)
-            addPoint()
+            addPoint(activePlayer)
 
         } else {    // Player didn't find
 
             hideCards(activeCards)
+            changePlayer()
 
         }    
     }
