f87ed2b4d32295e2c230510d9a43fe3d38f42961
Ádryan Pablo <apdsom22@gmail.com>
2023-09-18 00:42:07 -0300

    Add verification to turn card only if it's not discovered

--- src/tools/turnCard.js
+++ src/tools/turnCard.js
@@ -1,6 +1,8 @@
 export default function turnCard(target) {
 
     const card = target.closest('.card-game')
-    card.classList.toggle('-active')
-    
+
+    // Turn card only if it's not discovered
+    if (!card.classList.contains('-discovered')) { card.classList.toggle('-active') }
+
 }
