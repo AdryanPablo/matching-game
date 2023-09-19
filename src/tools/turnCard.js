95bd9b9bb5c62a97a2c89845ddd7fef8751043be
Ádryan Pablo <apdsom22@gmail.com>
2023-09-19 11:11:00 -0300

    Add turnCard verification so that don't turn CardGame when it's already active

--- src/tools/turnCard.js
+++ src/tools/turnCard.js
@@ -3,6 +3,9 @@ export default function turnCard(target) {
     const card = target.closest('.card-game')
 
     // Turn card only if it's not discovered
-    if (!card.classList.contains('-discovered')) { card.classList.toggle('-active') }
-
+    if (!card.classList.contains('-active') && !card.classList.contains('-discovered')) {
+        
+        card.classList.toggle('-active')
+    
+    }
 }
