6262a9aa73fab4bfba6e8b07d5c9e06a36793590
Ádryan Pablo <apdsom22@gmail.com>
2023-09-19 14:31:08 -0300

    Create resetScores feature

---
+++ src/tools/resetScores.js
@@ -0,0 +1,7 @@
+export default function resetScores() {
+
+    const scorePanel = document.querySelectorAll('.score')
+
+    scorePanel.forEach((score) => { score.classList.remove('-active') })
+    
+}
--- src/tools/startGame.js
+++ src/tools/startGame.js
@@ -1,13 +1,14 @@
 import hideWinnerModal from './hideWinnerModal.js'
 import hideCards from './hideCards.js'
+import resetScores from './resetScores.js'
 
 export default function startGame() {
 
     hideWinnerModal()
     hideCards( document.querySelectorAll('.card-game.-discovered') )
+    resetScores()
     
     console.log('Embaralhar cartões')
-    console.log('Zerar pontos')
     console.log('Vez do player1')
 
 }
